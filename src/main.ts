79ac1c6f1afcbfbaa9fa37fe16b0477f30cb0bc1
Khai Bui <khaibx.kma@gmail.com>
2025-02-18 16:29:42 +0700

    update port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
+  const port = 3050;
   await app.listen(port);
   console.log('Application is running on the port ', port);
 }
